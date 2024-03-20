367160441c332f56ab7c56525bfc321ccb66229d
Jón Rúnar Helgason <dev@jonrh.is>
2024-03-20 12:35:30 -0700

    Fixed layout, center content

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <h2 className={`mb-3 text-2xl font-semibold`}>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="grid text-center lg:text-left">
+        <h1 className={`mb-3 text-2xl font-semibold text-center`}>
           Laufás Vín
-        </h2>
+        </h1>
 
-        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        <p className={`m-0 max-w-[30ch] text-sm text-center opacity-50`}>
           Vín & Vínkynningar 🍷<br />
           laufasvin@laufasvin.is<br />
           846 7421<br />
